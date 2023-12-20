8205c4aded1848af10f0f8fc16650fc145830ccd
Alex Warden <146727849+AlexWarden2@users.noreply.github.com>
2023-12-20 15:23:05 +0000

    auth vol 2 for application

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const commentsController = require('../controllers/commentsController')
 
+const requireAuth = require('../middleware/requireAuth')
+router.use(requireAuth)
+
 router.get('/', commentsController.index)
 router.get('/:id', commentsController.getComment)
 router.post('/', commentsController.create)
